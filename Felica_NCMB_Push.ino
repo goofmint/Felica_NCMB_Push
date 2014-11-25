1b8b1eec6493d881bd52323af717d00c3e64a27f
Administrator <admin@example.com>
2014-11-25 22:38:32 +0900

    add README and License

---
+++ Felica_NCMB_Push.ino
@@ -0,0 +1,196 @@
+/****************************************************************
+WebClient.ino
+CC3000 WebClient Test
+Shawn Hymel @ SparkFun Electronics
+March 1, 2014
+https://github.com/sparkfun/SFE_CC3000_Library
+
+Manually connects to a WiFi network and performs an HTTP GET
+request on a web page. Prints the contents of the page to
+the serial console.
+
+The security mode is defined by one of the following:
+WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA, WLAN_SEC_WPA2
+
+Hardware Connections:
+ 
+ Uno Pin    CC3000 Board    Function
+ 
+ +5V        VCC or +5V      5V
+ GND        GND             GND
+ 2          INT             Interrupt
+ 7          EN              WiFi Enable
+ 10         CS              SPI Chip Select
+ 11         MOSI            SPI MOSI
+ 12         MISO            SPI MISO
+ 13         SCK             SPI Clock
+
+Resources:
+Include SPI.h, SFE_CC3000.h, and SFE_CC3000_Client.h
+
+Development environment specifics:
+Written in Arduino 1.0.5
+Tested with Arduino UNO R3
+
+This code is beerware; if you see me (or any other SparkFun 
+employee) at the local, and you've found our code helpful, please
+buy us a round!
+
+Distributed as-is; no warranty is given.
+****************************************************************/
+
+#include <SPI.h>
+#include <SFE_CC3000.h>
+#include <SFE_CC3000_Client.h>
+
+// Pins
+#define CC3000_INT      2   // Needs to be an interrupt pin (D2/D3)
+#define CC3000_EN       7   // Can be any digital pin
+#define CC3000_CS       10  // Preferred is pin 10 on Uno
+
+#include <RCS620S.h>
+#include <inttypes.h>
+#include <string.h>
+
+#define COMMAND_TIMEOUT 400
+#define POLLING_INTERVAL 500
+#define LED_PIN 13
+
+// Connection info data lengths
+#define IP_ADDR_LEN     4   // Length of IP address in bytes
+
+// Constants
+char ap_ssid[] = "your_ssid";                  // SSID of network
+char ap_password[] = "your_password";          // Password of network
+unsigned int ap_security = WLAN_SEC_WPA2; // Security of network
+unsigned int timeout = 30000;             // Milliseconds
+char server[] = "your_server_path";        // Remote host site
+RCS620S rcs620s;
+
+// Global Variables
+SFE_CC3000 wifi = SFE_CC3000(CC3000_INT, CC3000_EN, CC3000_CS);
+SFE_CC3000_Client client = SFE_CC3000_Client(wifi);
+
+const int LED = 13;
+const int BUTTON = 8;
+int connecting = 0;
+int val = 0;
+
+void setup() {
+  
+  ConnectionInfo connection_info;
+  int i;
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  
+  // Initialize Serial port
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("---------------------------");
+  Serial.println("SparkFun CC3000 - WebClient");
+  Serial.println("---------------------------");
+  
+  // Initialize CC3000 (configure SPI communications)
+  if ( wifi.init() ) {
+    Serial.println("CC3000 initialization complete");
+  } else {
+    Serial.println("Something went wrong during CC3000 init!");
+  }
+  
+  // Connect using DHCP
+  Serial.print("Connecting to SSID: ");
+  Serial.println(ap_ssid);
+  if(!wifi.connect(ap_ssid, ap_security, ap_password, timeout)) {
+    Serial.println("Error: Could not connect to AP");
+  }
+  
+  // Gather connection details and print IP address
+  if ( !wifi.getConnectionInfo(connection_info) ) {
+    Serial.println("Error: Could not obtain connection details");
+  } else {
+    Serial.print("IP Address: ");
+    for (i = 0; i < IP_ADDR_LEN; i++) {
+      Serial.print(connection_info.ip_address[i]);
+      if ( i < IP_ADDR_LEN - 1 ) {
+        Serial.print(".");
+      }
+    }
+    Serial.println();
+  }
+  int ret;
+  
+  pinMode(LED_PIN, OUTPUT);   // for Polling Status
+  digitalWrite(LED_PIN, LOW); 
+  
+  Serial.begin(115200);      // for RC-S620/S
+  Serial.println("Started");
+  // initialize RC-S620/S
+  ret = rcs620s.initDevice();
+  Serial.println(ret);
+  while (!ret) {}             // blocking
+  Serial.println("Initialized");
+}
+
+void get(String message) {
+  connecting = 1;
+  // Make a TCP connection to remote host
+  Serial.print("Performing HTTP GET of: ");
+  Serial.println(server);
+  if ( !client.connect(server, 80) ) {
+    Serial.println("Error: Could not make a TCP connection");
+  }  
+  // Make a HTTP GET request
+  client.println("GET /?m=" + message + " HTTP/1.1");
+  client.print("Host: ");
+  client.println(server);
+  client.println("Connection: close");
+  client.println();
+  Serial.println();
+  delay(3000);
+  while(client.available()){
+    char c = client.read();
+    Serial.print(c);
+    if ( !client.connected() ) {
+      Serial.println("disconnected.");
+      Serial.println();
+      //connecting = 0;
+      // Close socket
+      if ( !client.close() ) {
+        Serial.println("Error: Could not close socket");
+      }
+      
+      // Disconnect WiFi
+      /*
+      if ( !wifi.disconnect() ) {
+        Serial.println("Error: Could not disconnect from network");
+        
+      }
+      */
+      break;
+    }
+  }
+  client.close();
+  Serial.println("Finished WebClient test");
+}
+
+void loop() {
+  int ret, i;
+  String s = "";
+  // Polling
+  rcs620s.timeout = COMMAND_TIMEOUT;
+  ret = rcs620s.polling();
+
+  if(ret && connecting == 0) {
+    digitalWrite(LED_PIN, HIGH);
+    for(i = 0; i < 8; i++)
+    {
+      s.concat(rcs620s.idm[i]);
+    }
+    get(s);
+    digitalWrite(LED_PIN, LOW);
+    connecting = 0;
+  } else {
+  }
+  Serial.println("");
+  delay(POLLING_INTERVAL);
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Aruidno Felica Push
+
+ArduinoとFelicaリーダーを組み合わせて、読み取った内容をプッシュ通知するデモです。
+
+プッシュ通知には[ニフティクラウド mobile backend](http://mb.cloud.nifty.com/)を利用しています。
+
+なお実際のプッシュ通知作成は[プッシュサーバ](https://github.com/moongift/push_server/)で行っています。
+
+## LICENSE
+
+MIT License
+
